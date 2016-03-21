611b96c7a449b57fbd6bcec657945ab8c2f8dd8f
neliSun <nelson.chow91.nc@gmail.com>
2016-03-21 15:05:49 -0700

    Initial Commit

---
+++ ScrollViewImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGallery
+//
+//  Created by Nelson Chow on 2016-03-21.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewImageGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewImageGallery
+//
+//  Created by Nelson Chow on 2016-03-21.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
